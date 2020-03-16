aaf5b4e945c672cd74f2a084458bd26871428aa0
Aaron McGuire <samuel.aaron.mcguire@gmail.com>
2020-03-16 13:55:13 -0700

    Hacky fix for mobile browser resize

--- scrollmagic/uncompressed/ScrollMagic.js
+++ scrollmagic/uncompressed/ScrollMagic.js
@@ -33,12 +33,13 @@
 
 	ScrollMagic.version = "2.0.7";
 
+	console.log('Hacky ScrollMagic')
+
 	// TODO: temporary workaround for chrome's scroll jitter bug
 	window.addEventListener("mousewheel", function () {});
 
 	// global const
 	var PIN_SPACER_ATTRIBUTE = "data-scrollmagic-pin-spacer";
-
 	/**
 	 * The main class that is needed once per scroll container.
 	 *
@@ -159,8 +160,19 @@
 		 * Returns the current viewport Size (width vor horizontal, height for vertical)
 		 * @private
 		 */
+		ScrollMagic.mobileHeight = null;
 		var getViewportSize = function () {
-			return _options.vertical ? _util.get.height(_options.container) : _util.get.width(_options.container);
+
+			let _h = _options.vertical ? _util.get.height(_options.container) : _util.get.width(_options.container);
+			if(_util.isMobile()){
+				if(ScrollMagic.mobileHeight !== null){
+					return ScrollMagic.mobileHeight;
+				} else {
+					ScrollMagic.mobileHeight = _h;
+					return ScrollMagic.mobileHeight;
+				}
+			}
+			return _h
 		};
 
 		/**
@@ -2027,7 +2039,9 @@
 		 * @private
 		 */
 		var updatePinDimensions = function () {
+			
 			if (_pin && _controller && _pinOptions.inFlow) { // no spacerresize, if original position is absolute
+				
 				var
 					after = (_state === SCENE_STATE_AFTER),
 					before = (_state === SCENE_STATE_BEFORE),
@@ -2036,7 +2050,7 @@
 					pinTarget = _pinOptions.spacer.firstChild, // usually the pined element but can also be another spacer (cascaded pins)
 					marginCollapse = _util.isMarginCollapseType(_util.css(_pinOptions.spacer, "display")),
 					css = {};
-
+					// console.log("updatePinDimensions", before, after);
 				// set new size
 				// if relsize: spacer -> pin | else: pin -> spacer
 				if (_pinOptions.relSize.width || _pinOptions.relSize.autoFullWidth) {
@@ -2071,6 +2085,7 @@
 					css.height = during ? css["min-height"] : "auto";
 				}
 
+				
 				// add space for duration if pushFollowers is true
 				if (_pinOptions.pushFollowers) {
 					css["padding" + (vertical ? "Top" : "Left")] = _options.duration * _progress;
@@ -2272,9 +2287,13 @@
 			window.addEventListener('scroll', updatePinInContainer);
 			window.addEventListener('resize', updatePinInContainer);
 			window.addEventListener('resize', updateRelativePinSpacer);
+			window.addEventListener("orientationchange", () => {
+				ScrollMagic.mobileHeight = null;
+			  });
 			// add mousewheel listener to catch scrolls over fixed elements
 			_pin.addEventListener("mousewheel", onMousewheelOverPin);
 			_pin.addEventListener("DOMMouseScroll", onMousewheelOverPin);
+			
 
 			log(3, "added pin");
 
@@ -2584,7 +2603,22 @@
 			}
 			return obj;
 		};
-
+		
+		U.isMobile = function() {
+			var isMobile = false; //initiate as false
+			// device detection
+			if (
+			  /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+				navigator.userAgent
+			  ) ||
+			  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+				navigator.userAgent.substr(0, 4)
+			  )
+			) {
+			  isMobile = true;
+			}
+			return isMobile;
+		  };
 		// check if a css display type results in margin-collapse or not
 		U.isMarginCollapseType = function (str) {
 			return ["block", "flex", "list-item", "table", "-webkit-box"].indexOf(str) > -1;
